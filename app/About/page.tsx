4f2fdd6b346d94193f72295afff8ba9f879eb468
Muhammad Shariq <muhammadshariqfazal@gmail.com>
2024-12-20 15:54:50 +0500

    portfolio

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -13,7 +13,7 @@ const About = () => {
         {/* Section 1 */}
         <div className='p-[80px] flex justify-between w-full border-[#484848] border-b-[1px]'>
         <h1 className='bebas text-[101px]'>ABOUT ME</h1>
-        <div className='manrope w-[704px] flex flex-col gap-[20px]'>
+        <div className='manrope w-[600px] flex flex-col gap-[20px]'>
           <h2 className='text-[32px] leading-[140%]'>Aspiring Generative AI Engineer focused on growth and innovation.</h2>
           <p>I&apos;m Muhammad Shariq, a passionate learner with a keen interest in technology and problem-solving. Aspiring to become a Generative AI Engineer, I focus on mastering modern tools and building innovative solutions. I strive to grow both personally and professionally by taking on challenges that push my limits, aiming to make a meaningful impact through my work.</p>
 
--- app/page.tsx
+++ app/page.tsx
@@ -7,119 +7,119 @@ import Header from '@/components/Header';
 
 const Home = () => {
   return (
-    <div className='xl:min-w-[1440px]'>
+    <div className='xl:min-w-[1440px] sm:min-w-[375px]'>
 
       <Header />
       
       {/* Section 1 */}
-      <div className='p-[80px] flex justify-between w-full items-center border-[#484848] border-b-[1px]'>
+      <div className='p-[80px] flex xl:flex-row justify-between w-full items-center border-[#484848] border-b-[1px] sm:flex-col sm:gap-[30px] sm:px-[15px]'>
 
         {/* Left */}
-        <div className='flex flex-col gap-[20px] justify-center items-start w-[630px]'>
-          <h1 className='text-white text-[101px] bebas leading-[90%] '>HI, I AM <br /> MUHAMMAD SHARIQ.</h1>
-          <p className='text-[#c7c7c7] text-[18px] manrope'>Passionate learner striving to grow in Cloud Applied Generative AI and Full Stack Web Development.</p>
-          <div className='flex justify-start items-center gap-[16px]'>
-            <Link href=''><button type='button' className='bg-[#D3E97A]  manrope rounded-[50px] px-[24px] py-[20px] font-bold text-black'>CONTACT ME <FontAwesomeIcon icon={faCircle} className='size-[12px] inline-flex ml-[15px]' /> </button></Link>
+        <div className='flex flex-col gap-[20px] justify-center items-start xl:w-[630px] sm:w-[343px]'>
+          <h1 className='text-white xl:text-[101px] bebas leading-[90%] sm:text-[57px]'>HI, I AM <br /> MUHAMMAD SHARIQ.</h1>
+          <p className='text-[#c7c7c7] xl:text-[18px] manrope sm:text-[16px]'>Passionate learner striving to grow in Cloud Applied Generative AI and Full Stack Web Development.</p>
+          <div className='flex justify-start items-center xl:gap-[16px] sm:gap-[10px]'>
+            <Link href=''><button type='button' className='bg-[#D3E97A]  manrope rounded-[50px] px-[24px] py-[20px] font-bold text-black sm:text-[14px] xl:text-[16px]'>CONTACT ME <FontAwesomeIcon icon={faCircle} className='xl:size-[12px] inline-flex ml-[15px] sm:size-[10px]' /> </button></Link>
             <Link href='' className='bg-[#222222] rounded-[50px] p-[20px]'><FontAwesomeIcon icon={faQuora} className='size-[26px] text-[#D3E97A]' /></Link>
             <Link href='' className='bg-[#222222] rounded-[50px] p-[20px]'><FontAwesomeIcon icon={faGithub} className='size-[26px] text-[#D3E97A]' /></Link>
           </div>
         </div>
 
         {/* Right */}
-        <div className='w-[600px] h-[700px] bg-[#c7c7c7] rounded-[10px]'></div>
+        <div className='w-[600px] h-[700px] bg-[#c7c7c7] rounded-[10px] sm:w-[343px] sm:h-[400px]'></div>
       </div>
 
       {/* Section 2 */}
-      <div id='work' className='p-[80px] border-[#484848] border-b-[1px]'>
-        <h1 className='text-[78px] bebas text-[#c7c7c7]'>FEATURED PROJECTS</h1>
-        <p className='text-[#c7c7c7] text-[18px] manrope w-[600px]'>Here are some of the selected projects that showcase my passion for front-end development.</p>
+      <div id='work' className='p-[80px] border-[#484848] border-b-[1px] sm:px-[15px]'>
+        <h1 className='xl:text-[78px] bebas text-[#c7c7c7] sm:text-[57px] sm:leading-[90%]'>FEATURED PROJECTS</h1>
+        <p className='text-[#c7c7c7] xl:text-[18px] manrope xl:w-[600px] sm:text-[16px] sm:w-[343px]'>Here are some of the selected projects that showcase my passion for front-end development.</p>
 
         {/* 1 */}
-        <div className='flex justify-between w-full items-center my-[50px]'>
+        <div className='flex xl:flex-row justify-between w-full items-center my-[50px] sm:flex-col sm:gap-[30px]'>
 
           {/* Left */}
-          <div className='w-[600px] bg-[#1a1a1a] h-[600px] rounded-[10px]'>
+          <div className='xl:w-[600px] bg-[#1a1a1a] xl:h-[600px] rounded-[10px] sm:w-[343px] sm:h-[400px]'>
 
           </div>
 
           {/* Right */}
-          <div className='p-[50px] w-[600px] flex flex-col gap-[20px] manrope'>
-            <h1 className='text-white text-[32px]  leading-[140%] font-medium'>Craft Your Future, One Resume at a Time!</h1>
-            <p className=' text-[18px] text-[#c7c7c7] leading-[150%]'>Build, edit, and share your perfect resume effortlessly with our dynamic resume builder. Designed for all professionals, it combines simplicity, customization, and responsiveness to help you stand out. Take charge of your career story and make every application unforgettable!</p>
-            <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px] w-[576px]'>
+          <div className='xl:p-[50px] xl:w-[600px] flex flex-col gap-[20px] manrope sm:px-[10px] sm:w-[343px]'>
+            <h1 className='text-white xl:text-[32px] sm:text-[24px] leading-[140%] font-medium'>Craft Your Future, One Resume at a Time!</h1>
+            <p className=' xl:text-[18px] text-[#c7c7c7] leading-[150%] sm:text-[14px]'>Build, edit, and share your perfect resume effortlessly with our dynamic resume builder. Designed for all professionals, it combines simplicity, customization, and responsiveness to help you stand out. Take charge of your career story and make every application unforgettable!</p>
+            <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px] xl:w-[576px] sm:w-[343px]'>
               <h3 className='font-semibold  text-white py-[10px]'>PROJECT INFO</h3>
             </div>
-            <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px] w-[576px]'>
+            <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px] xl:w-[576px] sm:w-[343px]'>
               <h3 className='font-semibold  text-white py-[10px]'>Year</h3>
               <p className='text-[#c7c7c7] '>2024</p>
             </div>
-            <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px] w-[576px]'>
+            <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px] xl:w-[576px] sm:w-[343px]'>
               <h3 className='font-semibold  text-white py-[10px]'>Role</h3>
               <p className='text-[#c7c7c7] '>Front-end Developement</p>
             </div>
-            <div className='flex justify-start items-center gap-[24px] text-[#D3E97A] w-[576px]'>
-              <Link href='https://shariq-resume-builder-giaic.vercel.app' target='_blank' className='font-bold py-[5px] border-[#D3E97A] border-b-[2px]'>LIVE DEMO <FontAwesomeIcon icon={faArrowUpRightFromSquare} className='size-[15px] inline-flex items-start ml-[8px] mb-[4px]'/></Link>
-              <Link href='https://github.com/shariq001/GIAIC-Hackathon-1.git' target='_blank' className='font-bold py-[5px] border-[#D3E97A] border-b-[2px]'>SEE ON GITHUB <FontAwesomeIcon icon={faGithub} className='size-[15px] inline-flex items-start ml-[8px] mb-[4px]'/></Link>
+            <div className='flex justify-start items-center gap-[24px] text-[#D3E97A] xl:w-[576px] sm:w-[343px]'>
+              <Link href='https://shariq-resume-builder-giaic.vercel.app' target='_blank' className='font-bold py-[5px] border-[#D3E97A] border-b-[2px] xl:text-[16px] sm:text-[14px]'>LIVE DEMO <FontAwesomeIcon icon={faArrowUpRightFromSquare} className='size-[15px] inline-flex items-start ml-[8px] mb-[4px]'/></Link>
+              <Link href='https://github.com/shariq001/GIAIC-Hackathon-1.git' target='_blank' className='font-bold py-[5px] border-[#D3E97A] border-b-[2px] xl:text-[16px] sm:text-[14px]'>SEE ON GITHUB <FontAwesomeIcon icon={faGithub} className='size-[15px] inline-flex items-start ml-[8px] mb-[4px]'/></Link>
             </div>
           </div>
         </div>
 
         {/* 2 */}
-        <div className='flex justify-between w-full items-center my-[50px]'>
+        <div className='flex xl:flex-row justify-between w-full items-center my-[50px]'>
 
           {/* Left */}
-          <div className='w-[600px] bg-[#1a1a1a] h-[600px] rounded-[10px]'>
+          <div className='xl:w-[600px] bg-[#1a1a1a] xl:h-[600px] rounded-[10px] sm:w-[343px] sm:h-[400px]'>
 
           </div>
 
           {/* Right */}
-          <div className='p-[50px] w-[600px] flex flex-col gap-[20px] manrope'>
-            <h1 className='text-white text-[32px]  leading-[140%] font-medium'>Designed Clean and Seamless Login & Signup Page</h1>
-            <p className=' text-[18px] text-[#c7c7c7] leading-[150%]'>This project features beautifully designed login and signup pages with a focus on simplicity and functionality. The pages are interlinked for a smooth user experience, offering an intuitive and responsive design. Ideal for providing users with a hassle-free account creation and access process.</p>
-            <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px] w-[576px]'>
+          <div className='xl:p-[50px] xl:w-[600px] flex flex-col gap-[20px] manrope sm:px-[10px] sm:w-[343px]'>
+            <h1 className='text-white xl:text-[32px] sm:text-[24px] leading-[140%] font-medium'>Designed Clean and Seamless Login & Signup Page</h1>
+            <p className=' xl:text-[18px] sm:text-[14px] text-[#c7c7c7] leading-[150%]'>This project features beautifully designed login and signup pages with a focus on simplicity and functionality. The pages are interlinked for a smooth user experience, offering an intuitive and responsive design. Ideal for providing users with a hassle-free account creation and access process.</p>
+            <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px] xl:w-[576px] sm:w-[343px]'>
               <h3 className='font-semibold  text-white py-[10px]'>PROJECT INFO</h3>
             </div>
-            <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px] w-[576px]'>
+            <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px] xl:w-[576px] sm:w-[343px]'>
               <h3 className='font-semibold  text-white py-[10px]'>Year</h3>
               <p className='text-[#c7c7c7] '>2024</p>
             </div>
-            <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px] w-[576px]'>
+            <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px]xl:w-[576px] sm:w-[343px]'>
               <h3 className='font-semibold  text-white py-[10px]'>Role</h3>
               <p className='text-[#c7c7c7] '>Web Designer</p>
             </div>
-            <div className='flex justify-start items-center gap-[24px] text-[#D3E97A] w-[576px]'>
-              <Link href='https://3-assignment-eta.vercel.app' target='_blank' className='font-bold py-[5px] border-[#D3E97A] border-b-[2px]'>LIVE DEMO <FontAwesomeIcon icon={faArrowUpRightFromSquare} className='size-[15px] inline-flex items-start ml-[8px] mb-[4px]'/></Link>
-              <Link href='https://github.com/shariq001/q2-assignments.git' target='_blank' className='font-bold py-[5px] border-[#D3E97A] border-b-[2px]'>SEE ON GITHUB <FontAwesomeIcon icon={faGithub} className='size-[15px] inline-flex items-start ml-[8px] mb-[4px]'/></Link>
+            <div className='flex justify-start items-center gap-[24px] text-[#D3E97A] xl:w-[576px] sm:w-[343px]'>
+              <Link href='https://3-assignment-eta.vercel.app' target='_blank' className='font-bold py-[5px] border-[#D3E97A] border-b-[2px] xl:text-[16px] sm:text-[14px]'>LIVE DEMO <FontAwesomeIcon icon={faArrowUpRightFromSquare} className='size-[15px] inline-flex items-start ml-[8px] mb-[4px]'/></Link>
+              <Link href='https://github.com/shariq001/q2-assignments.git' target='_blank' className='font-bold py-[5px] border-[#D3E97A] border-b-[2px] xl:text-[16px] sm:text-[14px]'>SEE ON GITHUB <FontAwesomeIcon icon={faGithub} className='size-[15px] inline-flex items-start ml-[8px] mb-[4px]'/></Link>
             </div>
           </div>
         </div>
 
         {/* 3 */}
-        <div className='flex justify-between w-full items-center my-[50px]'>
+        <div className='flex xl:flex-row justify-between w-full items-center my-[50px] sm:flex-col sm:gap-[30px]'>
 
           {/* Left */}
-          <div className='w-[600px] bg-[#1a1a1a] h-[600px] rounded-[10px]'>
+          <div className='xl:w-[600px] bg-[#1a1a1a] xl:h-[600px] rounded-[10px] sm:w-[343px] sm:h-[400px]'>
 
           </div>
 
           {/* Right */}
-          <div className='p-[50px] w-[600px] flex flex-col gap-[20px] manrope'>
-            <h1 className='text-white text-[32px]  leading-[140%] font-medium'>A Desktop Showcase of Creativity</h1>
-            <p className=' text-[18px] text-[#c7c7c7] leading-[150%]'>This project is a portfolio webpage designed exclusively for desktop view, highlighting a clean and visually engaging layout. It serves as a platform to showcase skills, projects, and achievements in a professional and organized manner. Focused on desktop users, the design ensures an optimal browsing experience with polished aesthetics and functionality.</p>
-            <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px] w-[576px]'>
+          <div className='xl:p-[50px] xl:w-[600px] flex flex-col gap-[20px] manrope sm:px-[10px] sm:w-[343px]'>
+            <h1 className='text-white xl:text-[32px] sm:text-[24px] leading-[140%] font-medium'>A Desktop Showcase of Creativity</h1>
+            <p className=' xl:text-[18px] text-[#c7c7c7] leading-[150%] sm:text-[16px]'>This project is a portfolio webpage designed exclusively for desktop view, highlighting a clean and visually engaging layout. It serves as a platform to showcase skills, projects, and achievements in a professional and organized manner. Focused on desktop users, the design ensures an optimal browsing experience with polished aesthetics and functionality.</p>
+            <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px] xl:w-[576px] sm:w-[343px]'>
               <h3 className='font-semibold  text-white py-[10px]'>PROJECT INFO</h3>
             </div>
-            <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px] w-[576px]'>
+            <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px]  xl:w-[576px] sm:w-[343px]'>
               <h3 className='font-semibold  text-white py-[10px]'>Year</h3>
               <p className='text-[#c7c7c7] '>2024</p>
             </div>
-            <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px] w-[576px]'>
+            <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px]  xl:w-[576px] sm:w-[343px]'>
               <h3 className='font-semibold  text-white py-[10px]'>Role</h3>
               <p className='text-[#c7c7c7] '>Front-end Developement</p>
             </div>
-            <div className='flex justify-start items-center gap-[24px] text-[#D3E97A] w-[576px]'>
-              <Link href='https://9-assignment.vercel.app' target='_blank' className='font-bold py-[5px] border-[#D3E97A] border-b-[2px]'>LIVE DEMO <FontAwesomeIcon icon={faArrowUpRightFromSquare} className='size-[15px] inline-flex items-start ml-[8px] mb-[4px]'/></Link>
-              <Link href='https://github.com/shariq001/q2-assignments.git' target='_blank' className='font-bold py-[5px] border-[#D3E97A] border-b-[2px]'>SEE ON GITHUB<FontAwesomeIcon icon={faGithub} className='size-[15px] inline-flex items-start ml-[8px] mb-[4px]'/></Link>
+            <div className='flex justify-start items-center gap-[24px] text-[#D3E97A]  xl:w-[576px] sm:w-[343px]'>
+              <Link href='https://9-assignment.vercel.app' target='_blank' className='font-bold py-[5px] border-[#D3E97A] border-b-[2px] xl:text-[16px] sm:text-[14px]'>LIVE DEMO <FontAwesomeIcon icon={faArrowUpRightFromSquare} className='size-[15px] inline-flex items-start ml-[8px] mb-[4px]'/></Link>
+              <Link href='https://github.com/shariq001/q2-assignments.git' target='_blank' className='font-bold py-[5px] border-[#D3E97A] border-b-[2px] xl:text-[16px] sm:text-[14px]'>SEE ON GITHUB<FontAwesomeIcon icon={faGithub} className='size-[15px] inline-flex items-start ml-[8px] mb-[4px]'/></Link>
             </div>
           </div>
         </div>
