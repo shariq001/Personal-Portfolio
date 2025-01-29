0bae66596648505aee4e250aa652ad695e816f7d
Muhammad Shariq <muhammadshariqfazal@gmail.com>
2025-01-29 19:40:39 +0500

    personal portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -15,10 +15,10 @@ const Home = () => {
       <Header />
       
       {/* Section 1 */}
-      <div className='xl:p-[80px] flex md:flex-row justify-between w-full md:items-center sm:items-start border-[#484848] border-b-[1px] sm:flex-col sm:gap-[30px] sm:px-[15px] sm:py-[50px] lg:px-[50px]'>
+      <div className='xl:p-[80px] flex md:flex-row justify-between w-full md:items-center sm:items-start border-[#484848] border-b-[1px] sm:flex-col sm:gap-[30px] sm:px-[15px] sm:py-[50px] lg:px-[30px]'>
 
         {/* Left */}
-        <div className='flex flex-col gap-[20px] justify-center items-start xl:w-[630px] xl:max-w-[630px] sm:w-full '>
+        <div className='flex flex-col gap-[20px] justify-center items-start xl:w-[630px] sm:w-full '>
           <h1 className='text-white xl:text-[101px] bebas leading-[90%] sm:text-[57px]'>HI, I AM <br /> MUHAMMAD SHARIQ.</h1>
           <p className='text-[#c7c7c7] xl:text-[18px] manrope sm:text-[16px]'>Creative Front-End Developer | Passionate About Responsive Design</p>
           <div className='flex justify-start items-center xl:gap-[16px] sm:gap-[10px]'>
@@ -29,13 +29,13 @@ const Home = () => {
         </div>
 
         {/* Right */}
-        <div className='xl:w-[600px] xl:h-[700px] bg-[#1a1a1a] rounded-[10px] sm:w-full  sm:h-[400px] xl:px-[100px] xl:py-[80px] flex justify-center items-center'>
-          <Image src='/images/my-pic.jpeg' width={600} height={800} alt='My Pic' className='xl:w-[600px] xl:h-[800px] sm:min-w-[290px] sm:max-w-[300px] sm:h-[300px]'/>
+        <div className='xl:w-[500px] xl:h-[600px] bg-[#1a1a1a] rounded-[10px] sm:w-full  sm:h-[400px] xl:px-[100px] xl:py-[80px] flex justify-center items-center'>
+          <Image src='/images/my-pic.jpeg' width={600} height={800} alt='My Pic' className='xl:w-[400px] xl:h-[500px] sm:min-w-[290px] sm:max-w-[300px] sm:h-[300px]'/>
         </div>
       </div>
 
       {/* Section 2 */}
-      <div id='work' className='xl:p-[80px] border-[#484848] border-b-[1px] sm:px-[15px] sm:py-[50px] sm:flex sm:flex-col  xl:items-start lg:px-[50px]'>
+      <div id='work' className='xl:p-[80px] border-[#484848] border-b-[1px] sm:px-[15px] sm:py-[50px] sm:flex sm:flex-col  xl:items-start lg:px-[30px]'>
         <h1 className='xl:text-[78px] bebas text-[#c7c7c7] sm:text-[50px] sm:leading-[90%]'>FEATURED PROJECTS</h1>
         <p className='text-[#c7c7c7] xl:text-[18px] manrope xl:max-w-[600px] sm:text-[16px] sm:min-w-[290px]'>Here are some of the selected projects that <br className='sm:hidden md:block'/> showcase my passion for front-end development.</p>
 
@@ -43,12 +43,12 @@ const Home = () => {
         <div className='flex md:flex-row justify-between w-full items-center my-[50px] sm:flex-col sm:gap-[30px]'>
 
           {/* Left */}
-          <div className='xl:w-[600px] bg-[#1a1a1a] xl:h-[600px] rounded-[10px] sm:min-w-[290px] sm:h-[400px] xl:p-[0px] flex justify-center items-center sm:py-[80px] md:min-w-[369px]'>
+          <div className='xl:w-[600px] bg-[#1a1a1a] xl:h-[600px] rounded-[10px] sm:min-w-[290px] sm:h-[400px] xl:p-[0px] flex justify-center items-center sm:py-[80px] md:min-w-[369px] lg:min-w-[450px]'>
             <Image src='/images/project-1.png' width={600} height={600} alt='Project 1' className='xl:w-[600px] xl:h-[400px] rounded-[10px] sm:w-full sm:h-[250px]'/>
           </div>
 
           {/* Right */}
-          <div className='xl:p-[50px] xl:max-w-[600px] flex flex-col gap-[20px] manrope sm:px-[10px] sm:min-w-[290px] md:min-w-[369px]'>
+          <div className='xl:p-[50px] xl:max-w-[600px] flex flex-col gap-[20px] manrope sm:px-[10px] sm:min-w-[290px] md:min-w-[369px] lg:min-w-[450px]'>
             <h1 className='text-white xl:text-[32px] sm:text-[24px] leading-[140%] font-medium'>Building Modern Web Experiences.</h1>
             <p className=' xl:text-[18px] text-[#c7c7c7] leading-[150%] sm:text-[14px]'>Showcase your journey, skills, and achievements with this modern and responsive portfolio template. Built with Next.js and styled with Tailwind CSS, this design ensures speed, flexibility, and a stunning user experience. Customize it your way and let your work shine!</p>
             <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px] xl:w-[576px] sm:min-w-[290px]'>
@@ -73,12 +73,12 @@ const Home = () => {
         <div className='flex md:flex-row justify-between w-full items-center my-[50px] sm:flex-col sm:gap-[30px]'>
 
           {/* Left */}
-          <div className='xl:w-[600px] bg-[#1a1a1a] xl:h-[600px] rounded-[10px] sm:min-w-[290px] sm:h-[400px] xl:p-[0px] flex justify-center items-center sm:py-[90px] md:min-w-[369px]'>
+          <div className='xl:w-[600px] bg-[#1a1a1a] xl:h-[600px] rounded-[10px] sm:min-w-[290px] sm:h-[400px] xl:p-[0px] flex justify-center items-center sm:py-[90px] md:min-w-[369px] lg:min-w-[450px]'>
             <Image src='/images/project2.png' width={600} height={600} alt='Project 2' className='xl:w-[600px] xl:h-[400px] rounded-[10px] sm:w-full sm:h-[250px]'/>
           </div>
 
           {/* Right */}
-          <div className='xl:p-[50px] xl:max-w-[600px] flex flex-col gap-[20px] manrope sm:px-[10px] sm:min-w-[290px] md:min-w-[369px]'>
+          <div className='xl:p-[50px] xl:max-w-[600px] flex flex-col gap-[20px] manrope sm:px-[10px] sm:min-w-[290px] md:min-w-[369px] lg:min-w-[450px]'>
             <h1 className='text-white xl:text-[32px] sm:text-[24px] leading-[140%] font-medium'>Designed with Whitepace Precision.</h1>
             <p className=' xl:text-[18px] sm:text-[14px] text-[#c7c7c7] leading-[150%]'>Welcome to a fully responsive landing page that embraces the art of whitepace. Crafted using Next.js and Tailwind CSS, this design highlights clean layouts and balanced spaces, creating an elegant and modern user experience.</p>
             <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px] xl:w-[576px] sm:min-w-[290px]'>
@@ -103,12 +103,12 @@ const Home = () => {
         <div className='flex md:flex-row justify-between w-full items-center my-[50px] sm:flex-col sm:gap-[30px] '>
 
           {/* Left */}
-          <div className='xl:w-[600px] bg-[#1a1a1a] xl:h-[600px] rounded-[10px] sm:min-w-[290px] md:min-w-[369px] sm:h-[400px] xl:p-[0px] sm:py-[60px] flex justify-center items-center'>
+          <div className='xl:w-[600px] bg-[#1a1a1a] xl:h-[600px] rounded-[10px] sm:min-w-[290px] md:min-w-[369px] lg:min-w-[450px] sm:h-[400px] xl:p-[0px] sm:py-[60px] flex justify-center items-center'>
             <Image src='/images/project3.png' width={600} height={600} alt='Project 3' className='xl:w-[600px] xl:h-[350px] rounded-[10px] sm:min-w-[290px] md:min-w-[369px] sm:h-[250px]'/>
           </div>
 
           {/* Right */}
-          <div className='xl:p-[50px] xl:max-w-[600px] flex flex-col gap-[20px] manrope sm:px-[10px] sm:min-w-[290px] md:min-w-[369px]'>
+          <div className='xl:p-[50px] xl:max-w-[600px] flex flex-col gap-[20px] manrope sm:px-[10px] sm:min-w-[290px] md:min-w-[369px] lg:min-w-[450px]'>
             <h1 className='text-white xl:text-[32px] sm:text-[24px] leading-[140%] font-medium'>Drive Your Passion, Discover Your Ride.</h1>
             <p className=' xl:text-[18px] text-[#c7c7c7] leading-[150%] sm:text-[16px]'>This fully responsive PakWheels homepage clone recreates the sleek, user-friendly design of Pakistan’s leading car marketplace. Built with precision using Next.js and Tailwind CSS, it ensures a seamless browsing experience across all devices. Explore, admire, and customize to make it your own!</p>
             <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px] xl:w-[576px] sm:min-w-[290px]'>
@@ -133,12 +133,12 @@ const Home = () => {
         <div className='flex md:flex-row justify-between w-full items-center my-[50px] sm:flex-col sm:gap-[30px] '>
 
           {/* Left */}
-          <div className='xl:w-[600px] bg-[#1a1a1a] xl:h-[600px] rounded-[10px] sm:min-w-[290px] md:min-w-[369px] sm:h-[400px] xl:p-[0px] flex justify-center items-center sm:py-[60px]'>
+          <div className='xl:w-[600px] bg-[#1a1a1a] xl:h-[600px] rounded-[10px] sm:min-w-[290px] md:min-w-[369px] sm:h-[400px] xl:p-[0px] flex justify-center items-center sm:py-[60px] lg:min-w-[450px]'>
             <Image src='/images/project4.png' width={500} height={600} alt='Project 4' className='xl:w-[600px] xl:h-[400px] rounded-[10px] sm:min-w-[290px] md:min-w-[369px] sm:h-[250px]'/>
           </div>
 
           {/* Right */}
-          <div className='xl:p-[50px] xl:max-w-[600px] flex flex-col gap-[20px] manrope sm:px-[10px] sm:min-w-[290px] md:min-w-[369px]'>
+          <div className='xl:p-[50px] xl:max-w-[600px] flex flex-col gap-[20px] manrope sm:px-[10px] sm:min-w-[290px] md:min-w-[369px] lg:min-w-[450px]'>
             <h1 className='text-white xl:text-[32px] sm:text-[24px] leading-[140%] font-medium'>Your Portfolio, Your Story.</h1>
             <p className=' xl:text-[18px] text-[#c7c7c7] leading-[150%] sm:text-[16px]'>This minimalist portfolio design features a responsive Home and About page, perfect for introducing yourself and showcasing your journey. Built with Next.js and Tailwind CSS, it delivers a clean, modern look that adapts effortlessly to any screen size.</p>
             <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px] xl:w-[576px] sm:min-w-[290px] md:min-w-[369px]'>
@@ -163,12 +163,12 @@ const Home = () => {
         <div className='flex md:flex-row justify-between w-full items-center my-[50px] sm:flex-col sm:gap-[30px] '>
 
           {/* Left */}
-          <div className='xl:w-[600px] bg-[#1a1a1a] xl:h-[600px] rounded-[10px] sm:min-w-[290px] md:min-w-[369px] sm:h-[400px] xl:p-[0px] flex justify-center items-center sm:py-[60px]'>
+          <div className='xl:w-[600px] bg-[#1a1a1a] xl:h-[600px] rounded-[10px] sm:min-w-[290px] md:min-w-[369px] sm:h-[400px] xl:p-[0px] flex justify-center items-center sm:py-[60px] lg:min-w-[450px]'>
             <Image src='/images/project5.png' width={500} height={600} alt='Project 5' className='xl:w-[600px] xl:h-[400px] rounded-[10px] sm:min-w-[290px] md:min-w-[369px] sm:h-[250px]'/>
           </div>
 
           {/* Right */}
-          <div className='xl:p-[50px] xl:max-w-[600px] flex flex-col gap-[20px] manrope sm:px-[10px] sm:min-w-[290px] md:min-w-[369px]'>
+          <div className='xl:p-[50px] xl:max-w-[600px] flex flex-col gap-[20px] manrope sm:px-[10px] sm:min-w-[290px] md:min-w-[369px] lg:min-w-[450px]'>
             <h1 className='text-white xl:text-[32px] sm:text-[24px] leading-[140%] font-medium'>Shop the Design, Experience the Vision.</h1>
             <p className=' xl:text-[18px] text-[#c7c7c7] leading-[150%] sm:text-[16px]'>This fully responsive eCommerce website template is crafted with Next.js and Tailwind CSS. While not functional, it showcases a modern and sleek design, perfect for presenting products and creating a captivating shopping experience on any device.</p>
             <div className='flex justify-between items-center border-[#c7c7c7] border-b-[1px] xl:w-[576px] sm:min-w-[290px]'>
@@ -193,9 +193,9 @@ const Home = () => {
       </div>
 
       {/* Section 3 */}
-      <div id='about-sec' className='xl:p-[80px] flex md:flex-row xl:justify-between xl:items-center sm:items-start w-full border-[#484848] border-b-[1px] xl:h-[500px] sm:px-[15px] sm:py-[50px] sm:flex-col sm:gap-[30px] sm:justify-start lg:px-[50px]'>
-        <h1 className='bebas xl:text-[101px] sm:text-[43px] sm:min-w-[290px] md:min-w-[369px]'>ABOUT ME</h1>
-        <div className='manrope xl:max-w-[704px] flex flex-col gap-[20px] sm:min-w-[290px] md:min-w-[369px] '>
+      <div id='about-sec' className='xl:p-[80px] flex md:flex-row xl:justify-between xl:items-center sm:items-start w-full border-[#484848] border-b-[1px] xl:h-[500px] sm:px-[15px] sm:py-[50px] sm:flex-col sm:gap-[30px] sm:justify-start lg:px-[30px]'>
+        <h1 className='bebas xl:text-[101px] sm:text-[43px] sm:min-w-[290px] md:min-w-[369px] lg:min-w-[450px] xl:w-[600px]'>ABOUT ME</h1>
+        <div className='manrope xl:max-w-[600px] flex flex-col gap-[20px] sm:min-w-[290px] md:min-w-[369px] lg:min-w-[450px]'>
           <h2 className='xl:text-[32px] leading-[140%] sm:text-[24px]'>Aspiring Front-End Developer focused on creativity, education, and impactful projects.</h2>
           <p> Driven front-end developer with expertise in TypeScript, HTML, CSS, and frameworks like Next.js. Experienced in creating responsive, user-friendly interfaces using ShadCN and modern web technologies. Passionate about crafting high-quality, interactive web experiences while contributing to innovative projects. Always eager to enhance and reine front-end development skills in a collaborative environment. </p>
 
